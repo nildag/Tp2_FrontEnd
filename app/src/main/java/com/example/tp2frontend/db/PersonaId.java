02e423ac8b2f0184092652c9dd5bd8ca67b6fe39
nthprt <114790885+nthprt@users.noreply.github.com>
2023-11-12 19:42:56 -0300

    PersonaId y PersonaIdDao
    PersonaId para representar una clave foránea que se utiliza para relacionar tablas en la base de datos.

--- app/src/main/java/com/example/tp2frontend/db/PersonaId.java
+++ app/src/main/java/com/example/tp2frontend/db/PersonaId.java
@@ -1,9 +1,30 @@
 package com.example.tp2frontend.db;
 
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+@Entity
 public class PersonaId {
-    //aca creo que es foreingkey
+
     @PrimaryKey
     private Integer idPersona;
+
+    // Constructor vacío necesario para Room
+    public PersonaId() {}
+
+    // Constructor con parámetros para facilitar la creación de instancias
+    public PersonaId(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    // Getters y setters aquí...
+
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
 }
+
--- app/src/main/java/com/example/tp2frontend/db/PersonaIdDao.java
+++ app/src/main/java/com/example/tp2frontend/db/PersonaIdDao.java
@@ -1,12 +1,32 @@
 package com.example.tp2frontend.db;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Delete;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface PersonaIdDao {
 
-    /*public Integer getIdPersona() {
-        return idPersona;
-    }
+    @Insert
+    void insertPersonaId(PersonaId personaId);
 
-    public void setIdPersona(Integer idPersona) {
-        this.idPersona = idPersona;
-    }*/
+    @Query("SELECT * FROM personaid")
+    List<PersonaId> getAllPersonaIds();
+
+    @Query("SELECT * FROM personaid WHERE idPersona = :idPersona")
+    PersonaId getPersonaIdById(Integer idPersona);
+
+    @Delete
+    void deletePersonaId(PersonaId personaId);
+
+    @Update
+    void updatePersonaId(PersonaId personaId);
+
+    // Buscar una PersonaId por su idPersona
+    @Query("SELECT * FROM personaid WHERE idPersona = :idPersona")
+    PersonaId getPersonaIdById(Integer idPersona);
 }
